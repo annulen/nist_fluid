02df4ae9e9e1340b504aa1417f0cba1eea488cba
Konstantin Tokarev <annulen@yandex.ru>
2026-03-13 15:32:58 +0300

    Improve parse.pl

--- parse.pl
+++ parse.pl
@@ -9,10 +9,14 @@ use warnings;
 # my ($pdl1, $pdl2, $pdl3) = rcsv1D($csv_filename_or_filehandle, \@column_ids);
 # my $pdl = rcsv2D($csv_filename_or_filehandle, \@column_ids);
 
-my $pdl = rcsv2D('isotherm.txt', [1, 6], {sep_char => "\t"});
-my $sl = $pdl->slice('-1,:'); # T=298K, p=40 bar, S=?
+my $isotherm = rcsv2D('isotherm.txt', [1, 6], {sep_char => "\t"});
+my $sl = $isotherm->slice('-1,:'); # T=298K, p=40 bar, S=?
 print $sl;
-#my ($p, $S) = $pdl->slice('-1, :');
+#my ($p, $S) = $isotherm->slice('-1, :');
 my $S = $sl->index2d(0, 1);
 print "S=$S\n";
+
+
+my $sat_p = rcsv2D('satp.txt', [0, 1, 6], {sep_char => "\t"});
+print $sat_p;
 
