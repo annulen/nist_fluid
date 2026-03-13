bb6071891a1d5c16b2f03a5cc39d83cff29286c7
Konstantin Tokarev <annulen@yandex.ru>
2026-03-13 20:26:08 +0300

    Add satT (pressure increments), parse it

--- parse.pl
+++ parse.pl
@@ -12,11 +12,14 @@ use warnings;
 
 my $isotherm = rcsv2D('isotherm.txt', [1, 6], {sep_char => "\t"});
 my $sl = $isotherm(-1, :); # T=298K, p=40 bar, S=?
-print $sl;
-my $S = $sl->at(0, 1);
-print "S=$S\n";
+my $target_S = $sl->at(0, 1);
+print "S=$target_S\n";
 
 
-my $sat_p = rcsv2D('satp.txt', [0, 1, 6], {sep_char => "\t"});
-print $sat_p;
+#my $sat_p = rcsv2D('satp.txt', [0, 1, 6], {sep_char => "\t"});
+#print $sat_p;
 
+#my $satT = rcsv2D('satT.txt', [0, 1, 6, 13], {header => 1, sep_char => "\t"});
+#print $satT;
+my $satT = rcols('satT.txt', [0, 1, 6], {INCLUDE => qr/\tvapor$/, COLSEP => "\t"});
+print $satT(:, 2);
